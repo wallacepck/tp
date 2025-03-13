f3ffe8e8f292d13eccdc8f45cd960a22c4d42e0f
wallacepck <142145699+wallacepck@users.noreply.github.com>
2025-03-13 09:14:27 +0800

    Fix format in AddCommand help string

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -35,7 +35,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney"
+            + PREFIX_TAG + "owesMoney "
             + PREFIX_ROLE + "TA";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
